d780563f78781058c719975db7ce2605353d2ed9
nightfalldust <nightfalldust@163.com>
2015-12-15 18:01:32 +0800

    end_1215

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,7 @@ from django.test import LiveServerTestCase
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -35,9 +36,11 @@ class NewVisitorTest(LiveServerTestCase):
 		#伊迪斯的爱好是使用假蝇作为饵钓鱼
 		inputbox.send_keys('Buy peacock feathers')
 
-		#她按回车键后，页面更新了
-		#待办事项中显示了“1.Buy peacock feathers”
+		#她按回车键后，被带到了一个新的URL
+		#这个页面的待办事项中显示了“1.Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegexpMatches(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		#页面中又出现了一个文本框，可以输入其他的待办事项
@@ -51,10 +54,34 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		#伊迪斯很想知道这个网站是否会记住她的清单
-		#她看到网站为她生成了一个唯一的url
-		#而且页面中有一些文字解说这个功能
-		self.fail('Finish the test!')
-		#她访问那个url，发现她的待办事项列表还在
+		#现在一个叫做弗朗西斯的新用户访问了网站
 
-		#她很满意，去睡觉了
+		##我们使用一个新的浏览器会话
+		##确保伊迪斯的信息不会从cookies中泄露出来
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# 佛朗西斯访问首页
+		# 页面中看不到伊迪斯的清单
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# 佛朗西斯输入一个新的待办事项，新建一个清单
+		# 他不像伊迪斯那样兴趣盎然
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# 佛朗西斯获得了他的唯一url
+		francis_list_url = self.browser.current_url
+		self.assertRegexpMatches(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# 这个页面还是没有伊迪斯的清单
+		page_text = self.browser.find_element_by_id('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		#两个人都很满意，去睡觉了
