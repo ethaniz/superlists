c7bcbf9158b9c9b25f7e61bc2e1ae477b49e9b14
nightfalldust <nightfalldust@163.com>
2015-12-17 15:48:50 +0800

    refactory for functional test

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,39 @@
+#-*- coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
+from unittest import skip
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+
+	def test_cannot_add_empty_list_items(self):
+		#伊迪斯访问首页，不小心提交了一个空待办事项
+		#输入框中没输入内容，她就按下了回车键
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+		#首页刷新，显示一个错误消息
+		#提示待办事项不能为空
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
+
+		#她输入一些文字，然后再次提交，这次没问题了
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.check_for_row_in_list_table('1: Buy milk')
+
+		#她有点调皮，又提交了一个空的待办事项
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+		#在清单页面她看到了一个类似的错误信息
+		self.check_for_row_in_list_table('1: Buy milk')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
+
+		#输入文字后就没有问题了
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Make tea')		
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,39 +1,15 @@
 #-*- coding:utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
 import unittest
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import sys
+from unittest import skip
 
-class NewVisitorTest(StaticLiveServerTestCase):
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return
-		super(NewVisitorTest, cls).setUpClass()
-		cls.server_url = cls.live_server_url
+class NewVisitorTest(FunctionalTest):
 
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super(NewVisitorTest, cls).tearDownClass()
-
-
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-
-	def tearDown(self):
-		self.browser.quit()
-		#self.browser.close()
-
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#伊迪斯听说了一个很酷的在线代办事项应用
@@ -109,25 +85,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		#两个人都很满意，去睡觉了
-
-	def test_layout_and_styling(self):
-		# 伊迪斯访问首页
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024, 768)
-
-		# 她看到输入框完美的居中显示
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10,
-		)
-
-		# 她新建了一个清单，看到输入框仍完美低居中显示
-		inputbox.send_keys('testing\n')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10,
-		)
